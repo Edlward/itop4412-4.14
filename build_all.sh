95d2309791d210a2c70e9f1d2b6d55cf16dc70c9
mhw <510492956@qq.com>
2019-06-17 21:04:10 +0800

    1.优化编译：帮助集中输出文件2.修复itop4412_defconfig不能用问题

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,19 @@
+#!/bin/bash
+if [ ! -f out ]
+then 
+	mkdir out
+fi
+
 cd u-boot-2017.11/u-boot/
 
 ./build.sh 
 
 cd ../..
+cp u-boot-2017.11/u-boot/u-boot-iTOP-4412.bin out/
 
 cd linux-4.14.2_iTop-4412_scp
 
 ./build_kernel.sh
+cd ../
+cp linux-4.14.2_iTop-4412_scp/arch/arm/boot/dts/exynos4412-itop-elite.dtb out/
+cp linux-4.14.2_iTop-4412_scp/arch/arm/boot/uImage out/
