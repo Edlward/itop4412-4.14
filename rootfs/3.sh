31f5d564ccd156f04494e7c5a5eff1c0485cfb66
mhw <matthew.ma@quectel.con>
2019-06-24 15:30:35 +0800

    1.构件debian9由armel变为armhf 2.修复debian源错误命令jessie，改为stretch

--- rootfs/3.sh
+++ rootfs/3.sh
@@ -3,5 +3,5 @@ adduser mhw
 systemctl enable serial-getty@ttySAC2.service
 echo "inet:x:3003:root" >> /etc/group
 echo "net_raw:x:3004:root" >> /etc/group
-echo "deb http://mirrors.tuna.tsinghua.edu.cn/debian jessie main non-free contrib" >> /etc/apt/sources.list
+echo "deb http://mirrors.tuna.tsinghua.edu.cn/debian stretch main non-free contrib" >> /etc/apt/sources.list
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
