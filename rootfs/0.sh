accc973d5c3f96a908b7beb8ac05eb584a3c974b
mhw <matthew.ma@quectel.con>
2019-06-26 16:54:21 +0800

    1.下载必要工具2.更改清华大学的下载源

--- rootfs/0.sh
+++ rootfs/0.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo debootstrap --arch=armhf --foreign stretch fs_debian_stretch http://ftp.cn.debian.org/debian/
+sudo apt-get install binfmt-support qemu qemu-user-static debootstrap
+sudo debootstrap --arch=armhf --foreign stretch fs_debian_stretch http://mirrors.tuna.tsinghua.edu.cn/debian
 sudo cp /usr/bin/qemu-arm-static fs_debian_stretch/usr/bin
 sudo DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true LC_ALL=C LANGUAGE=C LANG=C chroot fs_debian_stretch debootstrap/debootstrap --second-stage
